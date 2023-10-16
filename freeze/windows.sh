a38873586f70f9aaca25ac64b373af1937db62da
Peter Story <peter.garth.story@gmail.com>
2023-10-16 15:48:23 -0400

    Add script for freezing for Windows

---
+++ freeze/windows.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+set -ex
+
+# The top-level directory. Use Windows-style file-separators for pyinstaller.
+ROOT=`cygpath -w "$PWD"`
+
+# Use pyinstaller to generate the .exe
+cd src/pydiode
+pyinstaller \
+  --noconfirm \
+  --windowed \
+  --name "Diode Transfer" \
+  --icon "$ROOT\freeze\OneWay.png" \
+  gui.py
+cd "$ROOT"
