1d56f1ac67a1724da43ddeaaf9d51b1c37b823b5
Peter Story <peter.garth.story@gmail.com>
2024-03-12 09:13:27 -0400

    Terminate stuck pipelines (#8)
    In the receiver pipeline, pydiode listens on the network and sends
    data to tar. Suppose you start sending a large file, then cancel
    sending, then restart sending. The receiver's tar will exit before
    its pydiode, presumably because tar received the amount of data it
    expected. Since tar comes later in the pipeline, tar won't consume
    pydiode's STDOUT, which will prevent pydiode from exiting.
    
    The solution is to detect cases where processes later in the
    pipeline exit before processes earlier in the pipeline. If this
    happens, all the processes in the pipeline should be terminated.
    
    Fixes #5

---
+++ tests/test_gui.py
@@ -0,0 +1,19 @@
+import unittest
+
+from pydiode.gui import stuck_running
+
+
+class TestGUI(unittest.TestCase):
+    def test_stuck_running(self):
+        self.assertTrue(stuck_running([None, 0]))
+        self.assertTrue(stuck_running([None, None, 0]))
+        self.assertTrue(stuck_running([None, 0, None]))
+
+        self.assertFalse(stuck_running([0]))
+        self.assertFalse(stuck_running([None]))
+
+        self.assertFalse(stuck_running([0, 0]))
+        self.assertFalse(stuck_running([0, None]))
+        self.assertFalse(stuck_running([0, None, None]))
+        self.assertFalse(stuck_running([0, 0, None]))
+        self.assertFalse(stuck_running([None, None]))
