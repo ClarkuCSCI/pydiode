0bb8d6dd81735e77164edc30c622844542c52ac5
Peter Story <peter.garth.story@gmail.com>
2024-07-23 10:27:20 -0400

    Delete decrypted files
    For #21

--- tests/test_decrypt.py
+++ tests/test_decrypt.py
@@ -17,25 +17,40 @@ class TestDecrypt(unittest.TestCase):
         )
 
     def test_decrypt(self):
-        files = {
-            Path("1.txt.gpg"): "ABC",
-            Path("2.txt.gpg"): "DEF",
-            Path("3.txt.gpg"): "GHI",
+        # We have a secret key for all but the last file
+        encrypted_files = [
+            Path("1.txt.gpg"),
+            Path("2.txt.gpg"),
+            Path("3.txt.gpg"),
+            Path("msg.gpg"),
+        ]
+        decrypted_files = {
+            Path("1.txt"): "ABC",
+            Path("2.txt"): "DEF",
+            Path("3.txt"): "GHI",
         }
 
         with tempfile.TemporaryDirectory() as tmpdir:
             # Copy the encrypted files
-            for file in files:
+            for file in encrypted_files:
                 shutil.copy(PARENT_DIR / file, tmpdir)
 
             # Decrypt the files
             decrypt = subprocess.run(
                 [sys.executable, "-m", "pydiode.decrypt"],
-                input="\n".join([str(tmpdir / file) for file in files.keys()]),
+                input="\n".join([str(tmpdir / f) for f in encrypted_files]),
+                capture_output=True,
+                check=True,
                 text=True,
             )
 
             # Check the files were decrypted
-            for file, data in files.items():
-                with open(tmpdir / file.with_suffix(""), "r") as f:
+            for file, data in decrypted_files.items():
+                with open(tmpdir / file, "r") as f:
                     self.assertEqual(data, f.read().strip())
+
+            # The properly decrypted files should be deleted
+            for file in encrypted_files[:-1]:
+                self.assertFalse((tmpdir / file).exists())
+            # The file that wan't decrypted should exist
+            self.assertTrue((tmpdir / encrypted_files[-1]).exists())
