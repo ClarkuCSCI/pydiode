a7c94ca7a1dcab04fa55f6457acceadc88ccf66a
Patrick Ziegler <104460499+Patrick-Ziegler@users.noreply.github.com>
2025-04-01 13:53:56 -0400

    Update Receive.py to use a different event loop policy
    Updated receive.py to use a WindowsSelectorEventLoopPolicy when running on windows

--- src/pydiode/receive.py
+++ src/pydiode/receive.py
@@ -5,6 +5,12 @@ import sys
 
 from .common import log_packet, PACKET_HEADER
 
+# Set even loop policy for Windows
+# This is because python defaults to the ProactorEventLoop on windows, which does not work with Standard input/output
+# https://docs.python.org/3/library/asyncio-policy.html
+# well when running asyncio.StreamReader and asyncio.StreamWriter.
+if sys.platform == "win32":
+    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 
 class AsyncWriter:
     def __init__(self, queue, exit_code):
