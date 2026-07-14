115174287685557fbadbf8104ed9dd5aabae67a4
Peter Story <peter.garth.story@gmail.com>
2026-07-14 12:14:48 -0400

    Fix chunk duration logging
    For #52

--- src/pydiode/send.py
+++ src/pydiode/send.py
@@ -201,7 +201,7 @@ def _send_chunk(
     Send packets containing all the data in the chunk, possibly redundantly
     until the target number of packets is sent.
     """
-    start = time.monotonic()
+    chunk_start = time.monotonic()
     # Wrap the chunk bytes in a helper class
     c = Chunk(chunk, color, chunk_max_packets)
     # Send the data over the network
@@ -210,18 +210,18 @@ def _send_chunk(
         target_elapsed = chunk_duration / chunk_max_packets
         logging.debug(f"Send iteration {r + 1}/{redundancy}")
         for data in c:
-            start = time.monotonic()
+            packet_start = time.monotonic()
             transport.sendto(data)
             log_packet("Sent", data)
             if (color == b"R" or color == b"B") and packet_details is not None:
                 packet_details.append(data)
-            already_elapsed = time.monotonic() - start
+            already_elapsed = time.monotonic() - packet_start
             sleep_duration = target_elapsed - already_elapsed
             if sleep_duration > 0:
                 time.sleep(sleep_duration)
     logging.debug(
         f"Sent {color} chunk of length {len(chunk)} "
-        f"in {time.monotonic() - start:.5f} seconds"
+        f"in {time.monotonic() - chunk_start:.5f} seconds"
     )
 
 
